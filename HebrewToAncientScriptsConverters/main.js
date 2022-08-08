191e83cc5b7dcf9665d0bd4ac68c29bfedac3bca
Naufal Rizky Rahardian <naufalrizky.r@gmail.com>
2022-08-09 00:59:50 +0700

    reupload

---
+++ HebrewToAncientScriptsConverters/main.js
@@ -0,0 +1,358 @@
+"use strict";
+const diacritic = [];
+diacritic.push("׳");
+diacritic.push("״");
+diacritic.push("\'");
+diacritic.push("'");
+diacritic.push("֑");
+diacritic.push("֒");
+diacritic.push("֓");
+diacritic.push("֔");
+diacritic.push("֕");
+diacritic.push("֖");
+diacritic.push("֗");
+diacritic.push("֘");
+diacritic.push("֙");
+diacritic.push("֚");
+diacritic.push("֛");
+diacritic.push("֜");
+diacritic.push("֝");
+diacritic.push("֞");
+diacritic.push("֟");
+diacritic.push("֠");
+diacritic.push("֡");
+diacritic.push("֢");
+diacritic.push("֣");
+diacritic.push("֤");
+diacritic.push("֥");
+diacritic.push("֦");
+diacritic.push("֧");
+diacritic.push("֨");
+diacritic.push("֩");
+diacritic.push("֪");
+diacritic.push("֫");
+diacritic.push("֬");
+diacritic.push("֭");
+diacritic.push("֮");
+diacritic.push("֯");
+diacritic.push("ְ");
+diacritic.push("ֱ");
+diacritic.push("ֲ");
+diacritic.push("ֳ");
+diacritic.push("ִ");
+diacritic.push("ֵ");
+diacritic.push("ֶ");
+diacritic.push("ַ");
+diacritic.push("ָ");
+diacritic.push("ֹ");
+diacritic.push("ֺ");
+diacritic.push("ֻ");
+diacritic.push("ּ");
+diacritic.push("ֽ");
+diacritic.push("ֿ");
+diacritic.push("ׁ");
+diacritic.push("ׂ");
+diacritic.push("ׄ");
+diacritic.push("ׅ");
+diacritic.push("ׇ");
+diacritic.push("ﬞ");
+diacritic.push("ࠖ");
+diacritic.push("ࠗ");
+diacritic.push("࠘");
+diacritic.push("࠙");
+diacritic.push("ࠚ");
+diacritic.push("ࠛ");
+diacritic.push("ࠜ");
+diacritic.push("ࠝ");
+diacritic.push("ࠞ");
+diacritic.push("ࠟ");
+diacritic.push("ࠠ");
+diacritic.push("ࠡ");
+diacritic.push("ࠢ");
+diacritic.push("ࠣ");
+diacritic.push("ࠤ");
+diacritic.push("ࠥ");
+diacritic.push("ࠦ");
+diacritic.push("ࠧ");
+diacritic.push("ࠨ");
+diacritic.push("ࠩ");
+diacritic.push("ࠪ");
+diacritic.push("ࠫ");
+diacritic.push("ࠬ");
+diacritic.push("࠭");
+diacritic.push("ܑ");
+diacritic.push("ܰ");
+diacritic.push("ܱ");
+diacritic.push("ܲ");
+diacritic.push("ܳ");
+diacritic.push("ܴ");
+diacritic.push("ܵ");
+diacritic.push("ܶ");
+diacritic.push("ܷ");
+diacritic.push("ܸ");
+diacritic.push("ܹ");
+diacritic.push("ܺ");
+diacritic.push("ܻ");
+diacritic.push("ܼ");
+diacritic.push("ܽ");
+diacritic.push("ܾ");
+diacritic.push("ܿ");
+diacritic.push("݀");
+diacritic.push("݁");
+diacritic.push("݂");
+diacritic.push("݃");
+diacritic.push("݄");
+diacritic.push("݅");
+diacritic.push("݆");
+diacritic.push("݇");
+diacritic.push("݈");
+diacritic.push("݉");
+diacritic.push("݊");
+diacritic.push("̀");
+diacritic.push("́");
+diacritic.push("̂");
+diacritic.push("̃");
+diacritic.push("̄");
+diacritic.push("̅");
+diacritic.push("̆");
+diacritic.push("̇");
+diacritic.push("̈");
+diacritic.push("̉");
+diacritic.push("̊");
+diacritic.push("̋");
+diacritic.push("̌");
+diacritic.push("̍");
+diacritic.push("̎");
+diacritic.push("̏");
+diacritic.push("̐");
+diacritic.push("̑");
+diacritic.push("̒");
+diacritic.push("̓");
+diacritic.push("̔");
+diacritic.push("̕");
+diacritic.push("̖");
+diacritic.push("̗");
+diacritic.push("̘");
+diacritic.push("̙");
+diacritic.push("̚");
+diacritic.push("̛");
+diacritic.push("̜");
+diacritic.push("̝");
+diacritic.push("̞");
+diacritic.push("̟");
+diacritic.push("̠");
+diacritic.push("̡");
+diacritic.push("̢");
+diacritic.push("̣");
+diacritic.push("̤");
+diacritic.push("̥");
+diacritic.push("̦");
+diacritic.push("̧");
+diacritic.push("̨");
+diacritic.push("̩");
+diacritic.push("̪");
+diacritic.push("̫");
+diacritic.push("̬");
+diacritic.push("̭");
+diacritic.push("̮");
+diacritic.push("̯");
+diacritic.push("̰");
+diacritic.push("̱");
+diacritic.push("̲");
+diacritic.push("̳");
+diacritic.push("̴");
+diacritic.push("̵");
+diacritic.push("̶");
+diacritic.push("̷");
+diacritic.push("̸");
+diacritic.push("̹");
+diacritic.push("̺");
+diacritic.push("̻");
+diacritic.push("̼");
+diacritic.push("̽");
+diacritic.push("̾");
+diacritic.push("̿");
+diacritic.push("̀");
+diacritic.push("́");
+diacritic.push("͂");
+diacritic.push("̓");
+diacritic.push("̈́");
+diacritic.push("ͅ");
+diacritic.push("͆");
+diacritic.push("͇");
+diacritic.push("͈");
+diacritic.push("͉");
+diacritic.push("͊");
+diacritic.push("͋");
+diacritic.push("͌");
+diacritic.push("͍");
+diacritic.push("͎");
+diacritic.push("͐");
+diacritic.push("͑");
+diacritic.push("͒");
+diacritic.push("͓");
+diacritic.push("͔");
+diacritic.push("͕");
+diacritic.push("͖");
+diacritic.push("͗");
+diacritic.push("͘");
+diacritic.push("͙");
+diacritic.push("͚");
+diacritic.push("͛");
+diacritic.push("͜");
+diacritic.push("͝");
+diacritic.push("͞");
+diacritic.push("͟");
+diacritic.push("͠");
+diacritic.push("͡");
+diacritic.push("͢");
+const equiv1 = []; const equiv2 = [];
+equiv1.push("װ"); equiv2.push("וו");
+equiv1.push("ױ"); equiv2.push("וי");
+equiv1.push("ײ"); equiv2.push("יי");
+equiv1.push("ײַ"); equiv2.push("ייַ");
+equiv1.push("יִ"); equiv2.push("יִ");
+equiv1.push("ﬠ"); equiv2.push("ע");
+equiv1.push("ﬡ"); equiv2.push("א");
+equiv1.push("ﬢ"); equiv2.push("ד");
+equiv1.push("ﬣ"); equiv2.push("ה");
+equiv1.push("ﬤ"); equiv2.push("כ");
+equiv1.push("ﬥ"); equiv2.push("ל");
+equiv1.push("ﬦ"); equiv2.push("ם");
+equiv1.push("ﬧ"); equiv2.push("ר");
+equiv1.push("ﬨ"); equiv2.push("ת");
+equiv1.push("שׁ"); equiv2.push("שׁ");
+equiv1.push("שׂ"); equiv2.push("שׂ");
+equiv1.push("שּׁ"); equiv2.push("שּׁ");
+equiv1.push("שּׂ"); equiv2.push("שּׂ");
+equiv1.push("אַ"); equiv2.push("אַ");
+equiv1.push("אָ"); equiv2.push("אָ");
+equiv1.push("אּ"); equiv2.push("אּ");
+equiv1.push("בּ"); equiv2.push("בּ");
+equiv1.push("גּ"); equiv2.push("גּ");
+equiv1.push("דּ"); equiv2.push("דּ");
+equiv1.push("הּ"); equiv2.push("הּ");
+equiv1.push("וּ"); equiv2.push("וּ");
+equiv1.push("זּ"); equiv2.push("זּ");
+equiv1.push("טּ"); equiv2.push("טּ");
+equiv1.push("יּ"); equiv2.push("יּ");
+equiv1.push("ךּ"); equiv2.push("ךּ");
+equiv1.push("כּ"); equiv2.push("כּ");
+equiv1.push("לּ"); equiv2.push("לּ");
+equiv1.push("מּ"); equiv2.push("מּ");
+equiv1.push("נּ"); equiv2.push("נּ");
+equiv1.push("סּ"); equiv2.push("סּ");
+equiv1.push("ףּ"); equiv2.push("ףּ");
+equiv1.push("פּ"); equiv2.push("פּ");
+equiv1.push("צּ"); equiv2.push("צּ");
+equiv1.push("קּ"); equiv2.push("קּ");
+equiv1.push("רּ"); equiv2.push("רּ");
+equiv1.push("שּ"); equiv2.push("שּ");
+equiv1.push("תּ"); equiv2.push("תּ");
+equiv1.push("וֹ"); equiv2.push("וֹ");
+equiv1.push("בֿ"); equiv2.push("בֿ");
+equiv1.push("כֿ"); equiv2.push("כֿ");
+equiv1.push("פֿ"); equiv2.push("פֿ");
+equiv1.push("ﭏ"); equiv2.push("אל");
+function elements(letters1, letters2) {
+	$("#submit-suggestion").html("שלח");
+	$(".subtitle").eq(0).attr("id", "subtitle");
+	$(".intro.white-section-area-thing").eq(0).attr("id", "letterlist");
+	let letterlist = "";
+	for (const i of letters1.keys()) {
+		letterlist += "<h2>" + letters2[i] + " = " + letters1[i] + "</h2>";
+	}
+	$('#letterlist').attr("class", "white-section-area-thing").html(letterlist);
+	$("a[style='color:#444444']").attr("target", "_blank").attr("rel", "noopener noreferrer");
+}
+function css(script) {
+	function insertCSS(code) {
+		const style = $("<style type = 'text/css'></style>");
+		$("head").append(style);
+		$(style).html(code);
+	}
+	insertCSS(`
+		.white-section-area-thing a {
+			color: #00a3e1;
+		}
+		@media(min-width:0px) and (max-width:768px) {
+			.white-section-area-thing {
+				box-sizing: border-box;
+				margin-top: 20px;
+				width: 95%;
+			}
+		}
+	`);
+	function setFont(elementName) {
+		$(elementName).css("font-family", "Noto Sans, Noto Sans Hebrew, Noto Sans " + script);
+	}
+	$('#firsttext').css("padding", "13px").css("box-sizing", "border-box").css("direction", "rtl");
+	$('#secondtext').css("padding", "13px").css("box-sizing", "border-box").css("direction", "rtl");
+	$("#letterlist").css("text-align", "center").css("height", "auto").css("direction", "rtl");
+	setFont('#firsttext');
+	setFont('#secondtext');
+	setFont("#subtitle");
+	setFont("#letterlist");
+}
+const translatorLink = [];
+const firstTranslatorText = "<li><a rel='noopener noreferrer' href='https://lingojam.com/";
+const middleTranslatorText = "andHebrewletterswapconverter'>";
+const lastTranslatorText = " and Hebrew letter swap converter</a></li>";
+translatorLink.push([firstTranslatorText + "Aramaic" + middleTranslatorText + "Aramaic" + lastTranslatorText, "aramaic"]);
+translatorLink.push([firstTranslatorText + "Elymaic" + middleTranslatorText + "Elymaic" + lastTranslatorText, "elymaic"]);
+translatorLink.push([firstTranslatorText + "OldNorthArabian" + middleTranslatorText + "Old North Arabian" + lastTranslatorText, "oldnortharabian"]);
+translatorLink.push([firstTranslatorText + "OldSouthArabian" + middleTranslatorText + "Old South Arabian" + lastTranslatorText, "oldsoutharabian"]);
+translatorLink.push([firstTranslatorText + "Palmyrene" + middleTranslatorText + "Palmyrene" + lastTranslatorText, "palmyrene"]);
+translatorLink.push([firstTranslatorText + "Parthian" + middleTranslatorText + "Parthian" + lastTranslatorText, "parthian"]);
+translatorLink.push([firstTranslatorText + "Phoenician" + middleTranslatorText + "Phoenician" + lastTranslatorText, "phoenician"]);
+translatorLink.push([firstTranslatorText + "Samaritan" + middleTranslatorText + "Samaritan" + lastTranslatorText, "samaritan"]);
+translatorLink.push([firstTranslatorText + "Syriac" + middleTranslatorText + "Syriac" + lastTranslatorText, "syriac"]);
+translatorLink.push([firstTranslatorText + "Nabataean" + middleTranslatorText + "Nabataean" + lastTranslatorText, "nabataean"]);
+translatorLink.push([firstTranslatorText + "Mandaic" + middleTranslatorText + "Mandaic" + lastTranslatorText, "mandaic"]);
+translatorLink.push([firstTranslatorText + "Hatran" + middleTranslatorText + "Hatran" + lastTranslatorText, "hatran"]);
+translatorLink.sort();
+const totaltranslatorProcess = [];
+for (const i of translatorLink.keys()) {
+	totaltranslatorProcess.push(translatorLink[i][0]);
+}
+const totaltranslatorLink = totaltranslatorProcess.join("");
+const designer = "Designed by <a rel='noopener noreferrer' target='_blank' href='https://www.reddit.com/user/ekirahardian'>Naufal Rizky Rahardian</a>.";
+const scriptLink = "<a rel='noopener noreferrer' target='_blank' href='https://github.com/EkiRahardian/LingoJam/tree/main/HebrewToAncientScriptsConverters'>GitHub</a>";
+function description() {
+	let descriptionText = "<div id='descriptionText' class='white-section-area-thing'><p>Swaps/converts Hebrew script to ";
+	if (arguments.length === 1) {
+		descriptionText += `${arguments[0]} script and vice versa. ${designer}</p>`;
+	}
+	else {
+		descriptionText += `ancient scripts in the list. ${designer}</p>`;
+	}
+	descriptionText += `
+			<p>This translator uses custom scripts from ${scriptLink}.</p>
+			${note}
+			<p>List of translators:</p>
+			<ul>${totaltranslatorLink}</ul>
+			<p>If something is wrong, try to reset the cache.</p>
+		</div>
+	`;
+	$("#suggestion-area").after(descriptionText);
+}
+function replaceLetters(replace1, replace2, string) {
+	const index = replace1.indexOf(string);
+	if (index !== -1) {
+		string = string.replace(replace1[index], replace2[index]);
+	}
+	return string;
+}
+function main(jsAbjad) {
+	window.parent.postMessage(window.location.href, homePage);
+	loadScript(homePage + 'abjads/' + jsAbjad +'.js?v=' + cache, function() {
+		elements(letters1, letters2);
+		css(script);
+		description(script);
+		$('#firsttext').on('input', firstTextEvent);
+		$('#secondtext').on('input', secondTextEvent);
+		$("#firsttext").val("כל בני האדם נולדו בני חורין ושווים בערכם ובזכויותיהם. כולם חוננו בתבונה ובמצפון, לפיכך חובה עליהם לנהוג איש ברעהו ברוח של אחווה.");
+		firstTextEvent();
+		$(".subtitle").html(subtitle + " - ברוכים הבאים");
+	});
+}
